d0bd6feaeefe04582e319bbd2526e7ea2fe8aada
Alexander Tsiomakh <a.tsiomakh@gmail.com>
2024-06-11 21:37:38 +0300

    responsive done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
+import './App.scss';
+
 import Header from './components/Header/Header';
 import Gallery from './components/Gallery/Gallery';
 import Overall from './components/Overall/Overall';
 import Highlights from './components/Highlights/Highlights';
 import Itinerary from './components/Itinerary/Itinerary';
-
-import './App.scss';
 
 const App = () => {
 	return (
